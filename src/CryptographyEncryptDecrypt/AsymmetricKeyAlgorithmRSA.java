1f8a8cf3fc78cfe84d642c08fde31c2eabfae08f
unknown <vozermuonnam1@gmail.com>
2018-12-26 14:15:28 +0700

    Add Digital Signature

--- src/CryptographyEncryptDecrypt/AsymmetricKeyAlgorithmRSA.java
+++ src/CryptographyEncryptDecrypt/AsymmetricKeyAlgorithmRSA.java
@@ -26,90 +26,31 @@ import javax.crypto.NoSuchPaddingException;
 
 //refer https://viblo.asia/p/java-ma-hoa-va-giai-ma-voi-thuat-toan-rsa-bJzKmW3Xl9N#_ma-hoa-va-giai-ma-du-lieu-trong-javarsa--cach-thuc-hoat-dong-2
 public class AsymmetricKeyAlgorithmRSA {
-	public static File publicKeyFile = new File("D://client/publickey.rsa");
-	public static File privateKeyFile = new File("D://server/privatekey.rsa");
+	/*public static File publicKeyFile = new File("D://client/publickey.rsa");
+	public static File privateKeyFile = new File("D://server/privatekey.rsa");*/
 	
 	public static void main(String[] args) {
 		
-		try {
-			
-			// Create keypair using RSA algorithm, Other algorithm such as: "ECDSA"
-			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-			// Create a secire random number generator using the SHA1PRNG algorithm
-			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-			
-			//init keypair with 1024 bit
-			keyGen.initialize(1024, secureRandom);
-			
-			KeyPair generateKeyPair = keyGen.genKeyPair();
-			PublicKey publicKey = generateKeyPair.getPublic();
-			PrivateKey privateKey = generateKeyPair.getPrivate();
-			
-			if(!publicKeyFile.exists() || publicKeyFile.exists()) {
-			 publicKeyFile.delete();
-			 publicKeyFile.createNewFile();
-			}
-			if(!privateKeyFile.exists() || privateKeyFile.exists()) {
-			 privateKeyFile.delete();
-			 privateKeyFile.createNewFile();
-			}
-			
-			FileOutputStream fileOutputPublicKey = new FileOutputStream(publicKeyFile);
-			//write publicKey into publicKey.rsa file
-			fileOutputPublicKey.write(publicKey.getEncoded());
-			fileOutputPublicKey.close();
-			
-			FileOutputStream fileOutputPrivateKey = new FileOutputStream(privateKeyFile);
-			//write privateKey into privateKey.rsa file
-			fileOutputPrivateKey.write(privateKey.getEncoded());
-			fileOutputPrivateKey.close();
-			
-			System.out.println("Create key pair ok!!");
-			
-			// Now, Test
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("Enter the text to encrypt using publicKey and send to server: ");
-			String text = scanner.nextLine();
-			// start to encryt
-			System.out.println(text + " encrypted and can send through network securely ^^: \n");
-			System.out.println("Encrypted text is :" + encrypt(text));
-			
-			System.out.println("Server will use private key to decrypt and text will :" + decrypt(encrypt(text)));
-			
-		} catch (NoSuchAlgorithmException | IOException e) {
-			// TODO Auto-generated catch block;
-			e.printStackTrace();
-		}
+		PublicKey publicKey = GenerateKeyPairUtils.getPublicKey();
+		System.out.println("Public Key: " + publicKey.toString());
+		PrivateKey privateKey = GenerateKeyPairUtils.getPrivateKey();
+		System.out.println("Private Key: " + Base64.getEncoder().encodeToString(privateKey.toString().getBytes()));
+		
+		// Now, Test
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the text to encrypt using publicKey and send to server: ");
+		String text = scanner.nextLine();
+		// start to encryt
+		System.out.println(text + " encrypted and can send through network securely ^^: \n");
+		System.out.println("Encrypted text is :" + encrypt(text));
+		
+		System.out.println("Server will use private key to decrypt and text will :" + decrypt(encrypt(text)));
 
 	}
 	
 	public static String encrypt(String text) {
-		try {
-			// Read publickey.rsa file 
-			FileInputStream fileInputStream = new FileInputStream("D://client/publickey.rsa");
-			//create byte array with lengh of text of publickey.rsa: ex: 162
-			byte[] byteArray = new byte[fileInputStream.available()];
-			// insert text of publickey.rsa into byteArray variable
-			fileInputStream.read(byteArray);
-			fileInputStream.close();
-			
-			// Because characters in publickey.rsa is difficult to read
-			// ex: 
-			//0Ÿ0
-			//	*†H†÷
-			//  0‰ €<7ÁIç1pBï“ÕÊO°™>«BSŸÞ#€&qÇªŽ¹lÝ°'!«ê]]”büê¢•ö¾®â øuÜç~±WP$ñL»#º¥Åhœ.ü
-			//¼¿’ÔžjÉÆ­fßuIŒÈ^d.3JŽ¬µŒµµ˜àN“µ¡Ç8G:ƒdÊ~ç³ 
-			
-			// So we need to covert it for easing client/customer to read. Hence, I use X509EncodedKeySpec class to create
-			// new public key
-			
-			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(byteArray);
-			// Create KeyFactory object and generate new publickey, okay, I have new publickey, it is friendly to read, ex: 
-			//Sun RSA public key, 1024 bits
-			//modulus: 90049830872911235273959796075228044346603257188619403535996907487108395899321562857131791273339565933899851985727648797494999199895037554569929682698478784994681389125308066027278503024789238566177492363123800245292349850962191848116017399107108287372881785503626287307321171170091300739177557877978264692659
-			//public exponent: 65537
-			PublicKey generatePublic = KeyFactory.getInstance("RSA").generatePublic(x509EncodedKeySpec);
-			
+		try {		
+			PublicKey generatePublic = GenerateKeyPairUtils.getPublicKey();
 			//Now, we have friendly publickey, next step, I will encrypt the text using new friendly publickey.
 			// Create Cipher object with parameter ARS algorithm
 			Cipher cipher = Cipher.getInstance("RSA");
@@ -122,7 +63,7 @@ public class AsymmetricKeyAlgorithmRSA {
 			byte[] doFinal = cipher.doFinal(text.getBytes());
 			return Base64.getEncoder().encodeToString(doFinal);
 			
-		} catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
 			e.printStackTrace();
 		}
 		return null;
@@ -130,28 +71,8 @@ public class AsymmetricKeyAlgorithmRSA {
 	
 	public static String decrypt(String encryptedText) {
 		//For decryption we will be using private key 
-		try {
-			FileInputStream fileInputStream = new FileInputStream("D://server/privateKey.rsa");
-			byte[] byteArray = new byte[fileInputStream.available()];
-			fileInputStream.read(byteArray);
-			fileInputStream.close();
-			
-			// By default, the privateKey.rsa is generated in PKCS#8 format as below,
-			//	*†H†÷
-			// ‚a0‚]  ±DÖíX4óŽ7.àú{%%—(ª<&?Ž%=D1ø	Ê+¤6>
-			//ÀƒÕMÙ+‚º'/þr‡°H«ÅqN›…¨ÏçÛtvñEì¶\¯{wÿÌÁ#D÷IÄ+f5À˜Ò·ÊÐÑf 'otÿýƒg•nã¨ÿ…¤Æ•ÃßF‘Å  ¦“xÿ›ñ Ão u[¼ÔŸ=Útªˆ,ÙaGa¡÷¸È	`Óêæ-÷ã86+
-			//XûêD(â«·w»)ZÁNŽ6äu¿D5]héÝE¸Ý•kÓøÜÐ*Mj¯_QaÞY­ªåÐ»Žë]áNð‘Žç ÓÖ>ƒàšéÕÚŽ¨ÍN|ðeA í½1}€Ó,®×X#C(¢]øâ¥÷äÇêg™9RŒ¨ÂE¯{Êš!gb‰¯yRåÆQäw@/ÄJÏBbÑ0v÷A ¾³Æ¡(3ädìfN°J¡áåÒ
-			//TîÁ4( ³¹JYÜ}jƒÁtQÑÿßÆXáÉc‘ ÖÞ<êèmžO ¢#A ¹ö•\$6$:/P~dÕGY•l'€˜sa¨V’.»˜J6”¼ÈÐB¤°4r²Zõž.é 5Ô‰gYÊ§Ö,v‘‘@Ÿÿð!„žø&hƒ0‹Ã‡Œ1?Û~Ä’¨‘ü‘ìã…_H-Ðn‡C§|Z2Xyc‹Y›’Ì¶2Gß$¨ª¥O@ 8²á"ÁÑr×ˆÉÕQs`…þšlYÞ*M¿¾1^&þÄï-E‘h´.@¿$¬`F½:£ì½€ÕºÊÑUB^‰ÏM
-			
-			// So before I decrypt the encryptedText, I generate the private key from base64 encoded string using PKCS8EncodedKeySpec 
-			// friendly format, I will use PKCS8EncodedKeySpec class (corresponding X509EncodedKeySpec for publickey).
-			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(byteArray);
-			// Using KeyFactory to create the new private key
-			PrivateKey generatePrivate = KeyFactory.getInstance("RSA").generatePrivate(pkcs8EncodedKeySpec);
-			System.out.println("Private key from D://server folder is " + Base64.getEncoder().encodeToString(generatePrivate.toString().getBytes()));
-			//Now, new private will have format as below:
-			//c3VuLnNlY3VyaXR5LnJzYS5SU0FQcml2YXRlQ3J0S2V5SW1wbEBmZmZhMWY1ZQ==
-			
+		try {		
+			PrivateKey generatePrivate = GenerateKeyPairUtils.getPrivateKey();
 			//ok, now I have friendly private key, I will start to decrypt the encryptedText
 			Cipher cipher = Cipher.getInstance("RSA");
 			cipher.init(Cipher.DECRYPT_MODE, generatePrivate);
@@ -160,7 +81,7 @@ public class AsymmetricKeyAlgorithmRSA {
 			Base64.getEncoder().encodeToString(doFinal);
 			return new String(doFinal);
 			
-		} catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- src/CryptographyEncryptDecrypt/GenerateKeyPairUtils.java
+++ src/CryptographyEncryptDecrypt/GenerateKeyPairUtils.java
@@ -76,11 +76,29 @@ public class GenerateKeyPairUtils {
 	public static PublicKey getPublicKey() {
 		FileInputStream fileInputStream;
 		try {
+			// Read publickey.rsa file 
 			fileInputStream = new FileInputStream("D://client/publickey.rsa");
+			//create byte array with lengh of text of publickey.rsa: ex: 162
 			byte[] byteArray = new byte[fileInputStream.available()];
+			// insert text of publickey.rsa into byteArray variable
 			fileInputStream.read(byteArray);
 			fileInputStream.close();
+			
+			// Because characters in publickey.rsa is difficult to read
+			// ex: 
+			//0Ÿ0
+			//	*†H†÷
+			//  0‰ €<7ÁIç1pBï“ÕÊO°™>«BSŸÞ#€&qÇªŽ¹lÝ°'!«ê]]”büê¢•ö¾®â øuÜç~±WP$ñL»#º¥Åhœ.ü
+			//¼¿’ÔžjÉÆ­fßuIŒÈ^d.3JŽ¬µŒµµ˜àN“µ¡Ç8G:ƒdÊ~ç³ 
+						
+			// So we need to covert it for easing client/customer to read. Hence, I use X509EncodedKeySpec class to create
+			// new public key
+									
 			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(byteArray);
+			// Create KeyFactory object and generate new publickey, okay, I have new publickey, it is friendly to read, ex: 
+			//Sun RSA public key, 1024 bits
+			//modulus: 90049830872911235273959796075228044346603257188619403535996907487108395899321562857131791273339565933899851985727648797494999199895037554569929682698478784994681389125308066027278503024789238566177492363123800245292349850962191848116017399107108287372881785503626287307321171170091300739177557877978264692659
+			//public exponent: 65537
 			PublicKey generatePublic = KeyFactory.getInstance("RSA").generatePublic(x509EncodedKeySpec);
 
 			return generatePublic;
@@ -98,7 +116,21 @@ public class GenerateKeyPairUtils {
 			byte[] byteArray = new byte[fileInputStream.available()];
 			fileInputStream.read(byteArray);
 			fileInputStream.close();
+			
+			// By default, the privateKey.rsa is generated in PKCS#8 format as below,
+			//	*†H†÷
+		   // ‚a0‚]  ±DÖíX4óŽ7.àú{%%—(ª<&?Ž%=D1ø	Ê+¤6>
+		   //ÀƒÕMÙ+‚º'/þr‡°H«ÅqN›…¨ÏçÛtvñEì¶\¯{wÿÌÁ#D÷IÄ+f5À˜Ò·ÊÐÑf 'otÿýƒg•nã¨ÿ…¤Æ•ÃßF‘Å  ¦“xÿ›ñ Ão u[¼ÔŸ=Útªˆ,ÙaGa¡÷¸È	`Óêæ-÷ã86+
+		   //XûêD(â«·w»)ZÁNŽ6äu¿D5]héÝE¸Ý•kÓøÜÐ*Mj¯_QaÞY­ªåÐ»Žë]áNð‘Žç ÓÖ>ƒàšéÕÚŽ¨ÍN|ðeA í½1}€Ó,®×X#C(¢]øâ¥÷äÇêg™9RŒ¨ÂE¯{Êš!gb‰¯yRåÆQäw@/ÄJÏBbÑ0v÷A ¾³Æ¡(3ädìfN°J¡áåÒ
+		   //TîÁ4( ³¹JYÜ}jƒÁtQÑÿßÆXáÉc‘ ÖÞ<êèmžO ¢#A ¹ö•\$6$:/P~dÕGY•l'€˜sa¨V’.»˜J6”¼ÈÐB¤°4r²Zõž.é 5Ô‰gYÊ§Ö,v‘‘@Ÿÿð!„žø&hƒ0‹Ã‡Œ1?Û~Ä’¨‘ü‘ìã…_H-Ðn‡C§|Z2Xyc‹Y›’Ì¶2Gß$¨ª¥O@ 8²á"ÁÑr×ˆÉÕQs`…þšlYÞ*M¿¾1^&þÄï-E‘h´.@¿$¬`F½:£ì½€ÕºÊÑUB^‰ÏM
+						
+		   // So before I decrypt the encryptedText, I generate the private key from base64 encoded string using PKCS8EncodedKeySpec 
+		   // friendly format, I will use PKCS8EncodedKeySpec class (corresponding X509EncodedKeySpec for publickey).
+			
 			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(byteArray);
+			// Using KeyFactory to create the new private key
+			//Now, new private will have format as below:
+			//c3VuLnNlY3VyaXR5LnJzYS5SU0FQcml2YXRlQ3J0S2V5SW1wbEBmZmZhMWY1ZQ==
 			PrivateKey generatePrivate = KeyFactory.getInstance("RSA").generatePrivate(pkcs8EncodedKeySpec);
 			return generatePrivate;
 
